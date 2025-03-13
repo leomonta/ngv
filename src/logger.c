7d252480ff5e4595e6261fe3c6b6e22c7eff63c7
Leonard <remon.w.dev@gmail.com>
2025-03-13 22:41:08 +0100

    more aligned logging

--- src/logger.c
+++ src/logger.c
@@ -58,7 +58,7 @@ void logger(const logLevel ll, const char *file_name, const unsigned line_num, c
 
 	// [LOG_LEVEL] filename:line_num in func_name(...) message
 	// [  INFO ] test.c:60 in logger(...) this is a test message
-	printf("%s %s:%d " GRY "in" RESET " %s    " RESET, prefix, file_name, line_num, function_name);
+	printf("%s %-10.10s:%-4d " GRY "in" RESET " %-20.20s    " RESET, prefix, file_name, line_num, function_name);
 	vprintf(format, args);
 	fflush(stdout); // ensure printing even with no \n
 
